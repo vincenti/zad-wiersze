3c3e06ee8ec29a7ef90ff2fe7f9077143f1d2886
Malgorzata Golab <golabm236@gmail.com>
2015-03-15 21:47:51 +0000

    Carpe diem

---
+++ features/carpe-diem.feature
@@ -0,0 +1,5 @@
+Feature: Web pages
+
+  Scenario: Carpe diem
+    Given I am on "carpe-diem.html"
+    Then I should see "Więc łap dzień każdy"
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -125,6 +125,13 @@ public function hymnAction()
      * K
      */
 
+     /**
+     * @Route("/carpe-diem.html", name="carpe-diem")
+     */
+    public function carpeDiemAction()
+    {
+        return $this->render('default/carpe-diem.html.twig');
+    }
     /*
      * L
      */
